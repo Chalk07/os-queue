439a49d2c00283f4aa69fef0749c412209eb0cf9
Chalk07 <151471972+Chalk07@users.noreply.github.com>
2023-12-10 21:26:03 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'os'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/utils.lua',
+    'server/functions.lua',
+    'server/events.lua',
+}
+
+lua54 'yes'
