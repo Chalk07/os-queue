75dd6d93e9ee182f06e0708433d4d5ea16e48b51
Chalk07 <151471972+Chalk07@users.noreply.github.com>
2023-12-11 20:14:04 +0000

    Update utils.lua

--- server/utils.lua
+++ server/utils.lua
@@ -1,3 +1,4 @@
+-- Determines if the server is currently at full capacity based on the maximum number of clients allowed.
 function isServerFull()
     local maxClients = GetConvarInt('sv_maxclients', 32)
     local currentPlayerCount = #GetPlayers()
@@ -5,10 +6,12 @@ function isServerFull()
     return currentPlayerCount >= maxClients
 end
 
+-- Retrieves a specific type of identifier for a player, defaulting to 'license' if not specified.
 function getIdentifier(source, identifierType)
     return GetPlayerIdentifierByType(source, identifierType or 'license')
 end
 
+-- Determines a player's position in the queue based on their unique identifier.
 function getQueuePosition(queue, itemKey)
     for i, node in ipairs(queue.heap) do
         if node.item == itemKey then
