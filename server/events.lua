f814110a4d77dbe3fd6537cfbcd17338b40e3315
Chalk07 <151471972+Chalk07@users.noreply.github.com>
2023-12-13 21:25:15 +0000

    Update events.lua

--- server/events.lua
+++ server/events.lua
@@ -1,5 +1,3 @@
-local queue = createPriorityQueue()
-
 local function onPlayerConnecting(name, setKickReason, deferrals)
     local source = source
     local licenseIdentifier = getIdentifier(source, 'license')
@@ -7,28 +5,27 @@ local function onPlayerConnecting(name, setKickReason, deferrals)
     deferrals.defer()
 
     if isServerFull() then
-        deferrals.update("Connecting...")
-        enqueue(queue, licenseIdentifier, 0)
+        enqueue(licenseIdentifier, 0)
 
         CreateThread(function()
             while true do
                 -- check if the player has cancelled the connection
                 if not GetPlayerEndpoint(source) then
-                    dequeue(queue, licenseIdentifier)
+                    dequeue(licenseIdentifier)
                     deferrals.done()
                     break
                 end
 
                 -- check if the player is at the front of the queue and the server has space
-                local position = getQueuePosition(queue, licenseIdentifier)
+                local position = getQueuePosition(licenseIdentifier)
                 if position == 1 and not isServerFull() then
                     deferrals.done()
                     break
                 elseif position then
-                    local total = #queue.heap
-                    deferrals.update(string.format("You are %d out of %d players in queue.", position, total))
+                    local queueSize = getQueueSize()
+                    deferrals.update(string.format("You are %d out of %d players in queue.", position, queueSize))
                 end
-                Wait(5000)
+                Wait(3000)
             end
         end)
     else
@@ -41,7 +38,5 @@ AddEventHandler('playerConnecting', onPlayerConnecting)
 
 RegisterServerEvent('removeFromQueue')
 AddEventHandler('removeFromQueue', function()
-    local source = source
-    local licenseIdentifier = getIdentifier(source, 'license')
-    dequeue(queue, licenseIdentifier)
+    dequeue(licenseIdentifier)
 end)
