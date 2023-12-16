93ec26bfb6d87c1f66e9c751db13bb46c769c59f
Chalk07 <151471972+Chalk07@users.noreply.github.com>
2023-12-16 13:03:03 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,9 +1,1 @@
-CreateThread(function()
-    while true do
-        Wait(0)
-        if NetworkIsSessionStarted() then
-            TriggerServerEvent('removeFromQueue')
-            break
-        end
-    end
-end)
+
