68c078fa669e1b48dd13edf81c194c961038e33e
Chalk07 <151471972+Chalk07@users.noreply.github.com>
2023-12-16 22:27:42 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,16 @@ fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
 author 'os'
+description 'Five M Queue System.'
 version '1.0.0'
 
 server_scripts {
+    '@oxmysql/lib/MySQL.lua',
     'server/utils.lua',
     'server/queue.lua',
     'server/events.lua',
 }
+
+dependency 'oxmysql'
 
 lua54 'yes'
