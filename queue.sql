41fea33616215bd991a4ecd7afff2915b7e8535f
Chalk07 <151471972+Chalk07@users.noreply.github.com>
2023-12-16 19:51:09 +0000

    Update queue.sql

--- queue.sql
+++ queue.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `priority` (
-      `id` INT(11) NOT NULL AUTO_INCREMENT,
-      `license` VARCHAR(50) NOT NULL,
-      `priority_level` INT(10) NOT NULL DEFAULT '0',
-      PRIMARY KEY (`id`)
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `license` VARCHAR(50) NOT NULL,
+  `priority_level` INT(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
