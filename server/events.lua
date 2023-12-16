56c4b9c2b6b2048c25fa05ebeacc9627a80eca42
Chalk07 <151471972+Chalk07@users.noreply.github.com>
2023-12-16 13:02:28 +0000

    Update events.lua

--- server/events.lua
+++ server/events.lua
@@ -4,39 +4,36 @@ local function onPlayerConnecting(name, setKickReason, deferrals)
 
     deferrals.defer()
 
-    if isServerFull() then
-        enqueue(licenseIdentifier, 0)
+    enqueue(licenseIdentifier, 0)
 
-        CreateThread(function()
-            while true do
-                -- check if the player has cancelled the connection
-                if not GetPlayerEndpoint(source) then
-                    dequeue(licenseIdentifier)
-                    deferrals.done()
-                    break
-                end
+    CreateThread(function()
+        while true do
+            Wait(3000)
 
-                -- check if the player is at the front of the queue and the server has space
-                local position = getQueuePosition(licenseIdentifier)
-                if position == 1 and not isServerFull() then
-                    deferrals.done()
-                    break
-                elseif position then
-                    local queueSize = getQueueSize()
-                    deferrals.update(string.format("You are %d out of %d players in queue.", position, queueSize))
-                end
-                Wait(3000)
+            -- Check if the player has cancelled the connection
+            if not GetPlayerEndpoint(source) then
+                dequeue(licenseIdentifier)
+                deferrals.done()
+                break
             end
-        end)
-    else
-        deferrals.done()
-    end
+
+            -- Continuously check the player's position in the queue
+            local position = getQueuePosition(licenseIdentifier)
+            if position == 1 and not isServerFull() then
+                deferrals.done()
+                break
+            elseif position then
+                local queueSize = getQueueSize()
+                deferrals.update(string.format("You are %d out of %d players in queue.", position, queueSize))
+            end
+        end
+    end)
 end
 
 AddEventHandler('playerConnecting', onPlayerConnecting)
 
-
-RegisterServerEvent('removeFromQueue')
-AddEventHandler('removeFromQueue', function()
-    dequeue(licenseIdentifier)
+AddEventHandler('playerJoining', function()
+    local source = source
+    local licenseIdentifier = getIdentifier(source, 'license')
+    dequeue()
 end)
